3299bc451dbbbe16e49af5b19a0d9ae96ad4a7f9
Chandradeo Arya <chandradeoaryaarya@gmail.com>
2023-05-20 01:39:04 +0530

    adding docker code

--- todo.py
+++ todo.py
@@ -1,13 +1,11 @@
-# Import Flask modules
+
 from flask import Flask, jsonify, abort, request, make_response
 from flaskext.mysql import MySQL
 from flask_cors import CORS
 
-# Create an object named app
 app = Flask(__name__)
 CORS(app)
 
-# Configure mysql database
 app.config['MYSQL_DATABASE_HOST'] = 'todo-database-server'
 app.config['MYSQL_DATABASE_USER'] = 'chandra'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Chandra@123'
@@ -19,9 +17,6 @@ connection = mysql.connect()
 connection.autocommit(True)
 cursor = connection.cursor()
 
-# Write a function named `init_todo_db` which initializes the todo db
-# Create P table within sqlite db and populate with sample data
-# Execute the code below only once.
 def init_todo_db():
     drop_table = 'DROP TABLE IF EXISTS todo_db.todos;'
     todos_table = """
@@ -44,9 +39,6 @@ def init_todo_db():
     cursor.execute(todos_table)
     cursor.execute(data)
 
-# Write a function named `get_all_tasks` which gets all tasks from the todos table in the db,
-# and return result as list of dictionary 
-# `[{'task_id': 1, 'title':'XXXX', 'description': 'XXXXXX', 'is_done': 'Yes' or 'No'} ]`.
 def get_all_tasks():
     query = """
     SELECT * FROM todos;
@@ -56,9 +48,6 @@ def get_all_tasks():
     tasks =[{'task_id':row[0], 'title':row[1], 'description':row[2], 'is_done': bool(row[3])} for row in result]
     return tasks
 
-# Write a function named `find_task` which finds task using task_id from the todos table in the db,
-# and return result as list of dictionary 
-# `{'task_id': 1, 'title':'XXXX', 'description': 'XXXXXX', 'is_done': 'Yes' or 'No'}`.
 def find_task(id):
     query = f"""
     SELECT * FROM todos WHERE task_id={id};
@@ -70,9 +59,6 @@ def find_task(id):
         task = {'task_id':row[0], 'title':row[1], 'description':row[2], 'is_done': bool(row[3])}
     return task
 
-# Write a function named `insert_task` which inserts task into the todos table in the db,
-# and return the newly added task as dictionary 
-# `{'task_id': 1, 'title':'XXXX', 'description': 'XXXXXX', 'is_done': 'Yes' or 'No'}`.
 def insert_task(title, description):
     insert = f"""
     INSERT INTO todos (title, description)
@@ -87,9 +73,6 @@ def insert_task(title, description):
     row = cursor.fetchone()
     return {'task_id':row[0], 'title':row[1], 'description':row[2], 'is_done': bool(row[3])}
 
-# Write a function named `change_task` which updates task into the todos table in the db,
-# and return updated added task as dictionary 
-# `{'task_id': 1, 'title':'XXXX', 'description': 'XXXXXX', 'is_done': 'Yes' or 'No'}`.
 def change_task(task):
     update = f"""
     UPDATE todos
@@ -105,8 +88,6 @@ def change_task(task):
     row = cursor.fetchone()
     return {'task_id':row[0], 'title':row[1], 'description':row[2], 'is_done': bool(row[3])}
 
-# Write a function named `remove_task` which removes task from the todos table in the db,
-# and returns True if successfully deleted or False.
 def remove_task(task):
     delete = f"""
     DELETE FROM todos
@@ -121,22 +102,16 @@ def remove_task(task):
     row = cursor.fetchone()
     return True if row is None else False
 
-# Write a function named `home` which returns 'Welcome to the Callahan's To-Do API Service',
-# and assign to the static route of ('/')
 @app.route('/')
 def home():
     return "Welcome to to-do API Service"
 
 
-# Write a function named `get_tasks` which returns all tasks in JSON format for `GET`,
-# and assign to the static route of ('/todos')
 @app.route('/todos', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks':get_all_tasks()})
 
 
-# Write a function named `get_tasks` which returns the task with given task_id in JSON format for `GET`,
-# and assign to the static route of ('/todos/<int:task_id>')
 @app.route('/todos/<int:task_id>', methods = ['GET'])
 def get_task(task_id):
     task = find_task(task_id)
@@ -144,16 +119,12 @@ def get_task(task_id):
         abort(404)
     return jsonify({'task found': task})
 
-# Write a function named `add_task` which adds new task using `POST` methods,
-# and assign to the static route of ('/todos')
 @app.route('/todos', methods=['POST'])
 def add_task():
     if not request.json or not 'title' in request.json:
         abort(400)
     return jsonify({'newly added task':insert_task(request.json['title'], request.json.get('description', ''))}), 201
 
-# Write a function named `update_task` which updates an existing task using `PUT` method,
-# and assign to the static route of ('/todos/<int:task_id>')
 @app.route('/todos/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
     task = find_task(task_id)
@@ -166,8 +137,6 @@ def update_task(task_id):
     task['is_done'] = int(request.json.get('is_done', int(task['is_done'])))
     return jsonify({'updated task': change_task(task)})
 
-# Write a function named `delete_task` which updates an existing task using `DELETE` method,
-# and assign to the static route of ('/todos/<int:task_id>')
 @app.route('/todos/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
     task = find_task(task_id)
@@ -175,18 +144,15 @@ def delete_task(task_id):
         abort(404)
     return jsonify({'result':remove_task(task)})
 
-# Write a function named `not_found` for handling 404 errors which returns 'Not found' in JSON format.
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-# Write a function named `bad_request` for handling 400 errors which returns 'Bad Request' in JSON format.
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
 
-# Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__== '__main__':
     init_todo_db()
-    # app.run(debug=True)
+
     app.run(host='0.0.0.0', port=80)
